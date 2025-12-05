7b1767d5a5f300dc7795e4f6e7972fea71b48ff9
rohi16122006 <rohithyadavkb@gmail.com>
2025-12-05 23:51:22 +0530

    Add main application class with demo messages

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven Project!");
+        System.out.println("This is a Jenkins CI/CD build demonstration.");
+        System.out.println("Build successful!");
+    }
+}
